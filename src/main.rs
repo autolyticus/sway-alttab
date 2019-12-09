19c738d7cd34423ca7c40562bec55447cccc7069
reisub0 <reisub0@gmail.com>
2019-12-09 18:09:03 +0530

    Removed async
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,56 +2,51 @@ use std::sync::{Arc, Mutex};
 
 use swayipc::reply::Event::Window;
 use swayipc::reply::WindowChange;
-use swayipc::{block_on, Connection, EventType};
+use swayipc::EventIterator;
+use swayipc::{Connection, EventType};
 
 type Res<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-async fn get_current_focused_id() -> i64 {
-    Connection::new()
-        .await
-        .unwrap()
-        .get_tree()
-        .await
-        .unwrap()
+fn get_current_focused_id() -> Res<i64> {
+    Connection::new()?
+        .get_tree()?
         .find_focused_as_ref(|n| n.focused)
-        .unwrap()
-        .id
+        .map(|n| n.id)
+        .ok_or_else(|| Err("Failed to get current Focused ID").unwrap())
 }
 
 fn handle_signal(last_focused: &Arc<Mutex<i64>>) {
-    block_on(async {
-        Connection::new()
-            .await
-            .unwrap()
-            .run_command(format!(
-                "[con_id={}] focus",
-                (*last_focused).lock().unwrap()
-            ))
-            .await
-            .unwrap();
-    })
+    Connection::new()
+        .unwrap()
+        .run_command(format!(
+            "[con_id={}] focus",
+            (*last_focused).lock().unwrap()
+        ))
+        .unwrap();
+}
+
+fn get_events() -> Res<EventIterator> {
+    let subs = [EventType::Window];
+    let sub = Connection::new()?;
+    Ok(sub.subscribe(&subs)?)
 }
 
 fn main() -> Res<()> {
-    block_on(async {
-        let subs = [EventType::Window];
-        let sub = Connection::new().await?;
-        let mut events = sub.subscribe(&subs).await?;
+    let last_focus = Arc::new(Mutex::new(0));
+    let mut cur_focus = get_current_focused_id()?;
+    let clone = Arc::clone(&last_focus);
 
-        let last_focus = Arc::new(Mutex::new(0));
-        let mut cur_focus = get_current_focused_id().await;
-        let clone = Arc::clone(&last_focus);
+    unsafe { signal_hook::register(signal_hook::SIGUSR1, move || handle_signal(&clone))? };
 
-        unsafe { signal_hook::register(signal_hook::SIGUSR1, move || handle_signal(&clone))? };
-        loop {
-            let event = events.next().await?;
-            if let Window(ev) = event {
-                if ev.change == WindowChange::Focus {
-                    let mut last = last_focus.lock().unwrap();
-                    *last = cur_focus;
-                    cur_focus = ev.container.id;
-                }
+    let mut events = get_events()?;
+    loop {
+        let event = events.next();
+        if let Some(Ok(Window(ev))) = event {
+            if ev.change == WindowChange::Focus {
+                let mut last = last_focus.lock().unwrap();
+                *last = cur_focus;
+                cur_focus = ev.container.id;
             }
         }
-    })
+    }
 }
